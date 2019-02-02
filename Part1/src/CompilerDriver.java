51f68b45899bde46068dad02637c175bd0e08d5f
BemisJo92 <bidinc.33@gmail.com>
2019-02-01 17:04:18 -0700

    Driver set up to run with any input file.

--- Part1/src/CompilerDriver.java
+++ Part1/src/CompilerDriver.java
@@ -1,15 +1,27 @@
 import org.antlr.v4.runtime.*;
+import java.io.*;
 
-public class CompilerDriver {
-  public static void main(String[] args) throws Exception {
-    String src = "BEGIN END 3.14159 42 FOO";
-    Plum lexer = new Plum(new ANTLRInputStream(src));
-    while(true) {
-      Token token = lexer.nextToken();
-      if(token.getType() == Plum.EOF) {
-        break;
-      }
-      System.out.println(token.getType() + " :: " + token.getText());
+public class CompilerDriver 
+{
+    public static void main(String[] args) throws Exception
+    {
+        String fileName = args[0];
+        File file = new File(fileName);
+        FileInputStream fis = null;
+        try{
+            fis = new FileInputStream(file);
+            ANTLRInputStream stream = new ANTLRInputStream(fis);
+            Plum lexer = new Plum(stream);
+            while(true) 
+            {
+                Token token = lexer.nextToken();
+                if(token.getType() == Plum.EOF) 
+                {
+                    break;
+                }
+                System.out.println(token.getType() + " :: " + token.getText());
+
+            }
+        } catch(IOException e){}
     }
-  }
 }
