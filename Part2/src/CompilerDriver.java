a15b2a9da9f32fde34debc2dc591d3cc50f0f6d5
joel <hedwigg01@gmail.com>
2019-02-21 17:59:06 -0700

    rules/productions added in Plum.g4

--- Part2/src/CompilerDriver.java
+++ Part2/src/CompilerDriver.java
@@ -16,20 +16,35 @@ public class CompilerDriver
             ANTLRInputStream stream = new ANTLRInputStream(fis);
             Plum lexer = new Plum(stream);
             Vocabulary vocab = lexer.getVocabulary();
-            while(true)
-            {
-                Token token = lexer.nextToken();
-                if(token.getType() == Plum.EOF) 
-                {
-                    break;
-                }
 
-                String tokenType = vocab.getDisplayName(token.getType());
-                String tokenText = token.getText();
+            CommonTokenStream tokens = new CommonTokenStream(lexer); //initialize a common token stream from the lexer
+            PlumParser parser = new PlumParser(tokens);
+            parser.program();   //function with the same name as your top-level construct (probably program) on that parser to parse your input.
+            
+
+
+            ParseTree tree = parser.program();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.walk( new PlumWalker(), tree ); //plumwalker class?
+
+            //https://gist.github.com/mattmcd/5425206
+
+
+            
+            // while(true)
+            // {
+            //     Token token = lexer.nextToken();
+            //     if(token.getType() == Plum.EOF) 
+            //     {
+            //         break;
+            //     }
+
+            //     String tokenType = vocab.getDisplayName(token.getType());
+            //     String tokenText = token.getText();
                 
-                System.out.println("Token Type: " + tokenType);
-                System.out.println("Value: " + tokenText);
-            }
+            //     System.out.println("Token Type: " + tokenType);
+            //     System.out.println("Value: " + tokenText);
+            // }
         } catch(IOException e){}
     }
 }
